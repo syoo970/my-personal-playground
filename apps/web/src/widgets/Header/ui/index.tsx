9788dcd3f44e0c4fdfea3227aebef926d31a8a0b
syoo970 <syu9710@gmail.com>
2024-03-26 17:35:18 +0900

    style: mobile & desktop 헤더

--- apps/web/src/widgets/Header/ui/index.tsx
+++ apps/web/src/widgets/Header/ui/index.tsx
@@ -1,16 +1,26 @@
 // header style component(tailwind)
+"use client";
 import StyledHeader from "./StyledHeader";
 import RoutingButton from "../../../entities/RoutingButton/ui";
 import { routes } from "../route";
+import { useMobile } from "../model/mobile";
 
 const Header = () => {
+  const { isMobile } = useMobile();
+
   return (
     <StyledHeader>
       <nav>
-        <ul className="flex flex-col gap-2">
+        <ul className={`flex gap-2 ${isMobile ? "flex-col" : "w-[350px]"}`}>
           {routes.map((route) => (
             <RoutingButton key={route.path} href={route.path}>
-              {route.label}
+              <div
+                className={`w-full flex items-center ${
+                  isMobile ? "" : "justify-center"
+                }`}
+              >
+                {route.label}
+              </div>
             </RoutingButton>
           ))}
         </ul>
